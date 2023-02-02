c94c6a441496696bd3ca085286700f199a95a49d
pablooraamiro <122534399+pablooraamiro@users.noreply.github.com>
2023-02-02 13:52:23 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,73 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (enviado == 0) {
+        basic.pause(100)
+    }
+    if (true) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (true) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    radio.sendNumber(0)
+})
+function GANADOR (núm: number) {
+	
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(0)
+    enviado = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(0)
+    enviado = 0
+})
+let enviado = 0
+radio.setGroup(79)
+enviado = 0
 basic.forever(function () {
 	
 })
